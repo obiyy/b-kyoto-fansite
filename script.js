5046a7112dd170361df478716318064b10000a1b
obiyy <obino@m.livemania.net>
2025-07-06 10:31:23 +0000

    自己紹介カードメーカー作成

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-    // ハンバーガーメニューのトグル
-    document.addEventListener('DOMContentLoaded', function() {
-      const toggleBtn = document.querySelector('.menu-toggle');
-      const navMenu = document.querySelector('.nav-menu');
+// ハンバーガーメニューのトグル
+document.addEventListener('DOMContentLoaded', function() {
+  alert("ファンサイトへようこそ");
+  const toggleBtn = document.querySelector('.menu-toggle');
+  const navMenu = document.querySelector('.nav-menu');
 
-      toggleBtn.addEventListener('click', function() {
-        navMenu.classList.toggle('show');
-      });
-    });
+  toggleBtn.addEventListener('click', function() {
+    alert("ファンサイトへようこそ");
+    navMenu.classList.toggle('show');
+  });
+});
