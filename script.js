a9efc91848caa4313bb3dedef7403503640ce035
obiyy <obino@m.livemania.net>
2025-07-06 10:33:46 +0000

    dev

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 // ハンバーガーメニューのトグル
 document.addEventListener('DOMContentLoaded', function() {
-  alert("ファンサイトへようこそ");
   const toggleBtn = document.querySelector('.menu-toggle');
   const navMenu = document.querySelector('.nav-menu');
 
   toggleBtn.addEventListener('click', function() {
-    alert("ファンサイトへようこそ");
     navMenu.classList.toggle('show');
   });
 });
